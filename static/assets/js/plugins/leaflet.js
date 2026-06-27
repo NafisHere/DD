659582ea625b7104fedba6a944873c6d868caebd
Abujor Shabbib <abujorshabbib@gmail.com>
2026-06-27 15:00:48 +0600

    Switch from Mapbox to OpenStreetMap tiles

--- static/assets/js/plugins/leaflet.js
+++ static/assets/js/plugins/leaflet.js
@@ -5,15 +5,11 @@
         if ($('#map-basic').length) {
             var mymap = L.map('map-basic').setView([51.505, -0.09], 13);
 
-            L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-                maxZoom: 18,
-                attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-                    '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-                    'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-                id: 'mapbox/streets-v11',
-                tileSize: 512,
-                zoomOffset: -1
-            }).addTo(mymap);
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                    maxZoom: 19,
+                    attribution: '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
+                }).addTo(mymap);
+
 
             L.marker([51.5, -0.09]).addTo(mymap)
                 .bindPopup("<b>Hello world!</b><br />I am a popup.").openPopup();
